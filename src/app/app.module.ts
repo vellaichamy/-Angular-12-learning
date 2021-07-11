6774fb6d00fe2a00c5173c9454e3bd51296135f5
vellaichamy <uvellaichamy@gmail.com>
2021-07-11 22:18:57 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NotificationsManagerComponent } from './cc-inputs-outputs/notifications-manager/notifications-manager.component';
 import { NotificationsButtonComponent } from './cc-inputs-outputs/notifications-button/notifications-button.component';
+import { VersionControlComponent } from './cc-ng-on-changes/components/version-control/version-control.component';
+import { VcLogsComponent } from './cc-ng-on-changes/components/vc-logs/vc-logs.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NotificationsManagerComponent, NotificationsButtonComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NotificationsManagerComponent,
+    NotificationsButtonComponent,
+    VersionControlComponent,
+    VcLogsComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
