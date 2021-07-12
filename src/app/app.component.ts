ac2370bd823f567ad0fecbc89e42f646a1fc1492
vellaichamy <uvellaichamy@gmail.com>
2021-07-12 19:54:49 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component,ViewChild, VERSION } from '@angular/core';
+import { Component, ViewChild, VERSION } from '@angular/core';
 import { GalleryComponent } from './cc-view-child/gallery/gallery.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
   notificationsCount = 0;
   updateNotificationsCount(count: number) {
     this.notificationsCount = count;
   }
 
-  @ViewChild(GalleryComponent) gallery;
+  // @ViewChild(GalleryComponent) gallery;
 
   addNewPicture() {
-    this.gallery.pictures.unshift(this.gallery.generateImage());
+    // this.gallery.pictures.unshift(this.gallery.generateImage());
   }
 
   removeFirstPicture() {
-    this.gallery.pictures.shift();
+    // this.gallery.pictures.shift();
   }
 }
