5e75d12485303d18566d0e742828889c1b5aa8e1
Ugra <uvellaichamy@gmail.com>
2021-07-12 20:02:16 +0530

    add selectedCardType: SocialCardType;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, VERSION } from '@angular/core';
 import { GalleryComponent } from './cc-view-child/gallery/gallery.component';
+import { SocialCardType } from './ng-dynamic-components/constants/social-card-type';
 
 @Component({
   selector: 'my-app',
@@ -11,6 +12,14 @@ export class AppComponent {
   notificationsCount = 0;
   updateNotificationsCount(count: number) {
     this.notificationsCount = count;
+  }
+
+
+  selectedCardType: SocialCardType;
+  cardTypes = SocialCardType;
+
+  setCardType(type: SocialCardType) {
+    this.selectedCardType = type;
   }
 
   // @ViewChild(GalleryComponent) gallery;
