920acf63da1271a8318a605fb6a8d61f256f296f
vellaichamy <uvellaichamy@gmail.com>
2021-07-12 20:32:30 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { VcLogsComponent } from './cc-ng-on-changes/components/vc-logs/vc-logs.c
 import { VcLogsComponent1 } from './cc-setters/components/vc-logs/vc-logs.component';
 import { VersionControlComponent1 } from './cc-setters/components/version-control/version-control.component';
 import { GalleryComponent } from './cc-view-child/gallery/gallery.component';
+import { TwitterCardComponent } from './ng-dynamic-components/components/twitter-card/twitter-card.component';
 // import { GalleryComponent } from './cc-view-child/gallery/gallery.component';
 
 @NgModule({
@@ -25,7 +26,8 @@ import { GalleryComponent } from './cc-view-child/gallery/gallery.component';
     VersionControlComponent1,
     VcLogsComponent1,
     // ,
-    GalleryComponent
+    GalleryComponent,
+    TwitterCardComponent
   ],
   bootstrap: [AppComponent]
 })
--- src/app/ng-dynamic-components/components/social-card/social-card.component.ts
+++ src/app/ng-dynamic-components/components/social-card/social-card.component.ts
@@ -6,16 +6,19 @@ import {
   ComponentFactoryResolver,
   ViewChild,
   ViewContainerRef,
-  SimpleChanges,
+  SimpleChanges
 } from '@angular/core';
-import { SocialCardType } from 'src/app/constants/social-card-type';
+import { SocialCardType } from '../../constants/social-card-type';
 import { FbCardComponent } from '../fb-card/fb-card.component';
 import { TwitterCardComponent } from '../twitter-card/twitter-card.component';
+// import { SocialCardType } from 'src/app/constants/social-card-type';
+// import { FbCardComponent } from '../fb-card/fb-card.component';
+// import { TwitterCardComponent } from '../twitter-card/twitter-card.component';
 
 @Component({
   selector: 'app-social-card',
   templateUrl: './social-card.component.html',
-  styleUrls: ['./social-card.component.scss'],
+  styleUrls: ['./social-card.component.scss']
 })
 export class SocialCardComponent implements OnInit, OnChanges {
   @Input() type: SocialCardType;
@@ -41,8 +44,9 @@ export class SocialCardComponent implements OnInit, OnChanges {
         component = TwitterCardComponent;
         break;
     }
-    const componentFactory =
-      this.componentFactoryResolver.resolveComponentFactory(component);
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      component
+    );
     this.vrf.clear();
     this.vrf.createComponent(componentFactory);
   }
